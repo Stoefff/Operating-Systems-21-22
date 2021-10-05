6d9e1c22d7760480da4cd797973519cf8ba8b553
Stoefff <xxthespinerxx@gmail.com>
2021-10-05 21:51:10 +0300

    Cleaning of main branch

--- Week06/task1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-users=$(who | grep $1)
-for user in $users
-do
-    if test $user = $1
-    then echo $user is logged in
-    fi
-done
--- Week13/README.md
+++
@@ -1,3 +0,0 @@
-### Exercise - 12.01.21
-
-## Just questions and tasks
